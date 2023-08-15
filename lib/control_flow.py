904ddf724fe5f81b18fdc03637c057bcd0654365
peterkomo <mwangipeterkomo@gmail.com>
2023-08-15 14:29:19 +0300

    corrected everything

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,34 +1,24 @@
-#!/usr/bin/env python3
-
 def admin_login(username, password):
-    
-    if username =="admin" or username =="ADMIN" and password=="12345":
-         print("Access granted")
-         return "Acces granted"
-    else: 
-         
-         print("Access denied") 
-         return "Acces denied"  
-    
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        print("Access granted")
+        return "Access granted"
+    else:
+        print("Access denied")
+        return "Access denied"
 
 def hows_the_weather(temperature):
     if temperature < 40:
-        print("brisk")
-        return "brisk"
+        print("It's brisk out there!")
+        return "It's brisk out there!"
     elif temperature >= 40 and temperature <= 65:
-        print("a little chilly")
-        return "a little chilly"
+        print("It's a little chilly out there!")
+        return "It's a little chilly out there!"
     elif temperature > 85:
-        print("too dang hot")
-        return "too dang hot"
+        print("It's too dang hot out there!")
+        return "It's too dang hot out there!"
     else:
-        print("perfect")
-        return "perfect"
-
-
-
-    
-    
+        print("It's perfect out there!")
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
@@ -44,9 +34,6 @@ def fizzbuzz(num):
         print(num)
         return num
 
-    
-    
-
 def calculator(operation, num1, num2):
     if operation == "+":
         return num1 + num2
@@ -57,7 +44,8 @@ def calculator(operation, num1, num2):
     elif operation == "/":
         return num1 / num2
     else:
-         print("Invalid operation!")
-         return None
+        print("Invalid operation!")
+        return None
+
 
     
