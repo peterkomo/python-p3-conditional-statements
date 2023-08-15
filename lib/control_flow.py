29b303e432b0cea36979fc1d00e9ab47d49eac9b
peterkomo <mwangipeterkomo@gmail.com>
2023-08-15 14:22:30 +0300

    fixing bugs

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,36 +1,49 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    
     if username =="admin" or username =="ADMIN" and password=="12345":
-        print("Access granted")
+         print("Access granted")
+         return "Acces granted"
     else: 
-        print("Access denied")   
+         
+         print("Access denied") 
+         return "Acces denied"  
     
 
 def hows_the_weather(temperature):
     if temperature < 40:
         print("brisk")
-    elif temperature >=40 and temperature<=65:
+        return "brisk"
+    elif temperature >= 40 and temperature <= 65:
         print("a little chilly")
-    elif temperature >85:
+        return "a little chilly"
+    elif temperature > 85:
         print("too dang hot")
-    else : 
+        return "too dang hot"
+    else:
         print("perfect")
+        return "perfect"
 
 
-    # your code here
+
+    
     
 
 def fizzbuzz(num):
-    if num  %3==0 and num %5==0:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-    elif num  %3==0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
         print("Fizz")
-    elif num %5==0:
+        return "Fizz"
+    elif num % 5 == 0:
         print("Buzz")
+        return "Buzz"
     else:
-        print(num) 
+        print(num)
+        return num
+
     
     
 
@@ -44,7 +57,7 @@ def calculator(operation, num1, num2):
     elif operation == "/":
         return num1 / num2
     else:
-        print("Invalid operation!")
-        return None
+         print("Invalid operation!")
+         return None
 
     
